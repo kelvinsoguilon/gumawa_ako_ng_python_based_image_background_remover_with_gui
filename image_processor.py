28a034e337413bb3199da1a423b225c678cea8a1
Kelvin <soguilonkelvin@gmail.com>
2025-06-26 17:32:52 +0800

    Fixed self.image variable to encapsulated. Added the function to load image and also raise an error if file is not loaded properly

--- image_processor.py
+++ image_processor.py
@@ -2,5 +2,15 @@ from PIL import Image
 
 class ImageProcessor:
     def __init__(self):
-        self.image = None
+        self._image = None
         self.original_image_pil = None
+
+    def load_image(self, path):
+        try:
+            with open(path, "rb") as file:
+                self._image = file.read()
+            self.original_image_pil = Image.open(path).convert("RGBA")
+            return True
+        except Exception as error:
+            print(f"Error Loading image: {error}")
+            return False
