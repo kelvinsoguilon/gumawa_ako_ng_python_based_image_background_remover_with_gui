04a84f521a17c5bd440250c52d83f086a4ae6494
Kelvin <soguilonkelvin@gmail.com>
2025-06-26 20:25:22 +0800

    Imported necessary libraries and the background remover process file

--- main.py
+++ main.py
@@ -2,3 +2,9 @@
 #get image from os or files from desktop
 #remove the background of image
 #save image to desktop or any path you want
+
+import tkinter as tk
+from tkinter import messagebox, filedialog
+from PIL import Image, ImageTk
+import io
+from get_background_removed import BackgroundRemover
