2fc8c9da6fec6f9ba85873163cdc4e9d796e7d25
Kelvin <soguilonkelvin@gmail.com>
2025-06-26 17:48:15 +0800

    Worked on the background remover process by using rembg library, added error catcher if image is not processed

--- get_background_removed.py
+++ get_background_removed.py
@@ -1,2 +1,12 @@
 from image_processor import ImageProcessor
 from rembg import remove
+
+class BackgroundRemover:
+    def process_image(self):
+        if self._image:
+            try:
+                return remove(self.image)
+            except Exception as error:
+                print("Error processing image: ", error)
+                return None
+            return None
