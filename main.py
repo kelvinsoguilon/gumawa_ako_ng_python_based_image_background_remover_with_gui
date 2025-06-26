3909346f750568d0d651ae85be076e2989f810b1
Kelvin <soguilonkelvin@gmail.com>
2025-06-26 21:09:27 +0800

    Added choose image button and save button in UI

--- main.py
+++ main.py
@@ -45,3 +45,16 @@ class InterfaceBackgroundRemover:
         self.result_image_box = tk.Label(self.image_frame)
         self.result_image_box.grid(row=0, column=1, padx=30)
         
+        #Choose image and save button
+        self.button_frame = tk.Frame(root)
+        self.button_frame.pack(pady=20)
+
+        self.choose_button = tk.Button(root, text = "Choose Image", font = ("Arial", 12), command= self.choose_image)
+        self.choose_button.grid(row=0, column=0, padx= 30)
+
+        self.save_button = tk.Button(root, text = "Save Image", font = ("Arial", 12), command=self.save_image, state=tk.DISABLED)
+        self.save_button.grid(row = 0, column = 1, padx = 30)
+
+        self.result_image = None
+
+        
